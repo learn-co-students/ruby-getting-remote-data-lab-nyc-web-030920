e2dfdc41c0d999a5d400fbab4af4045b6a4f1ab3
github username <github email address>
2020-03-23 13:04:05 -0400

    commit

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,18 +1,2 @@
 # Write your code here
-require 'net/http'
-require 'open-uri'
-require 'json'
-class GetRequester 
-    def initialize(url)
-        @url = url
-    end
-
-    def get_response_body
-        uri = URI.parse(@url)
-        resp = Net::HTTP.get_response(uri)
-        resp.body
-    end
-
-
-
-    def 
+ 
